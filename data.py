7597f021d1857c654ff67d6faaa56bc13f721c96
Gergely <milegergely@gmail.com>
2021-02-23 10:03:04 +0100

    Add sql cursor and password hash functions

--- data.py
+++ data.py
@@ -2,3 +2,23 @@ import util
 from psycopg2.extras import RealDictCursor, DictCursor
 from psycopg2 import sql
 import database_common
+
+@database_common.connection_handler
+def add_new_row(cursor: DictCursor, dictionary: dict, target_table: str):
+    value_list, column_list = util.generate_col_value_list(target_table, dictionary)
+    query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+        sql.Identifier(target_table),
+        sql.SQL(', ').join(column_list),
+        sql.SQL(', ').join([sql.Placeholder()] * len(value_list)))
+    cursor.execute(query, tuple(value_list))
+
+@database_common.connection_handler
+def get_row(cursor: RealDictCursor, table: str, text_id: int, col: str):
+    query = f"SELECT * FROM {table} WHERE {col} = '{text_id}'"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return data
+
+@database_common.connection_handler
+def delete_row(cursor: RealDictCursor, table: str, id: int):
+    cursor.execute(f"DELETE FROM {table} WHERE id = {id}")
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from flask import jsonify
+import bcrypt
+from psycopg2 import sql
 
 
 def json_response(func):
@@ -13,3 +15,22 @@ def json_response(func):
         return jsonify(func(*args, **kwargs))
 
     return decorated_function
+
+def generate_col_value_list(dictionary):
+    value_list = []
+    column_list = []
+    for column, value in dictionary.items():
+        column_list.append(sql.Identifier(column))
+        value_list.append(value)
+    return value_list, column_list
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
