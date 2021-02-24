1aef29a6a8f46d7375478b25b780cd793ead72ea
Gergely <milegergely@gmail.com>
2021-02-24 11:29:48 +0100

    Add refresh after post

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -70,6 +70,7 @@ export let dataHandler = {
     createNewCard: function (cardTitle, boardId, statusId, callback) {
         let data = {title: cardTitle, board_id: boardId, status_id: statusId};
         this._api_post('/create-card', data);
+        document.location.reload();
     }
     // here comes more features
 };
